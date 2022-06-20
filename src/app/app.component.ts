466937871e9d9bb339920fb546a75b270b19466c
Letto228 <learnjs@MacBook-Pro-Egor.local>
2022-06-20 22:09:21 +0300

    feat: lesson 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
 	title = 'Angular-learnjs-130622';
 	titleSecret = 'Angular-learnjs-130622-Secret';
+
+	imgSrc =
+		'https://get.wallhere.com/photo/landscape-sea-reflection-bridge-suspension-bridge-1680x1050-px-nonbuilding-structure-cable-stayed-bridge-truss-bridge-706446.jpg';
 }
