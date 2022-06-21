983a5c416ae936a0c2f8e2c7bce1896bf43d3253
Aleksandr Makushkin <makushkin.developer@yandex.ru>
2022-06-21 13:03:47 +0300

    feat: added div with background image

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 	title = 'Angular-learnjs-130622';
+
+	currentImagePath: String;
+	private _currentIndex = 0;
+	private _imagePaths = [
+		'https://w-dog.ru/wallpapers/9/16/520628925075361/afrika-yuzhnaya-afrika-namibiya-pejzazh-oblaka-zakat-pustynya.jpg',
+		'https://storge.pic2.me/c/1360x800/786/5286db5fd4678.jpg',
+		'https://karatu.ru/wp-content/uploads/2019/09/040919.jpg',
+	];
+
+	constructor() {
+		this.currentImagePath = this._imagePaths[this._currentIndex];
+
+		setInterval(() => {
+			this._currentIndex++;
+
+			if (this._currentIndex >= this._imagePaths.length) {
+				this._currentIndex = 0;
+			}
+
+			this.currentImagePath = this._imagePaths[this._currentIndex];
+		}, 2000);
+	}
 }
