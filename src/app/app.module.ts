806bd743f0e6cf3429c5c34a556876996b5ceb5c
Letto228 <learnjs@MacBook-Pro-Egor.local>
2022-06-23 19:22:21 +0300

    chore: fix header position

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HeaderModule } from './pages/header/header.module';
+import { HeaderModule } from './components/header/header.module';
 import { SidenavModule } from './components/sidenav/sidenav.module';
 
 @NgModule({
