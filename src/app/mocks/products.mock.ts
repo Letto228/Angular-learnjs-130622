8c108cd96236cae39d1b3b28d10b1fcc6f19f8ce
Letto228 <learnjs@MacBook-Pro-Egor.local>
2022-06-23 22:30:48 +0300

    feat: lesson 3 add interfaces and mocks

---
+++ src/app/mocks/products.mock.ts
@@ -0,0 +1,318 @@
+import { IProduct } from '../shared/interfaces/product.interface';
+
+export const productsMock: IProduct[] = [
+	{
+		_id: '96-planset-dexp-ursus-s290-32-gb-3g-cernyj',
+		feedbacksCount: 2,
+		subCategory: 'planshety-elektronnye-knigi',
+		images: [
+			{
+				url: 'https://c.dns-shop.ru/thumb/st1/fit/0/0/dff090e667cd39dd397678905df52de5/6c3e7aab802deb19513c7cf55dd7c527efb9127d8aba2ed90f3d224577137fe2.jpg',
+				source: '6c3e7aab802deb19513c7cf55dd7c527efb9127d8aba2ed90f3d224577137fe2.jpg',
+			},
+			{
+				url: 'https://c.dns-shop.ru/thumb/st4/fit/wm/2000/2000/2f06814e52f8c780fa094eb0c8607c97/9e36eded9a34407a9d962a5da5269fea6df2a3467ff103821f5e3d66791ef1b7.jpg',
+				source: '9e36eded9a34407a9d962a5da5269fea6df2a3467ff103821f5e3d66791ef1b7.jpg',
+			},
+			{
+				url: 'https://c.dns-shop.ru/thumb/st4/fit/wm/2000/2000/71b76ea354e44f1db0fe5eb21b1aa7d0/5b7d7d6fabaa7ed681c3aa9b050e1d29667c5e65fae923d932b480265a338a8f.jpg',
+				source: '5b7d7d6fabaa7ed681c3aa9b050e1d29667c5e65fae923d932b480265a338a8f.jpg',
+			},
+			{
+				url: 'https://c.dns-shop.ru/thumb/st1/fit/wm/2000/2000/4158bd2cdb5298bf30147ee158fc3f24/a8a469b0ae9088c5ad0b966aec1b25a445d3900aa17373980cd1de69f51dd376.jpg',
+				source: 'a8a469b0ae9088c5ad0b966aec1b25a445d3900aa17373980cd1de69f51dd376.jpg',
+			},
+			{
+				url: 'https://c.dns-shop.ru/thumb/st4/fit/wm/2000/2000/c7234ce61bc2ed05eaa457d737ff42e5/be0a67c8db96314d0c0166f2522a15f7bff8814751b36c4038c9232e71f71b1a.jpg',
+				source: 'be0a67c8db96314d0c0166f2522a15f7bff8814751b36c4038c9232e71f71b1a.jpg',
+			},
+			{
+				url: 'https://c.dns-shop.ru/thumb/st4/fit/wm/2000/2000/289bb1ee285d64fce2069763d1d0b9e0/4894c23d8b72014e31ca6d4f8e47a03947a01173b53ad3b58d44eeef4a0c96b8.jpg',
+				source: '4894c23d8b72014e31ca6d4f8e47a03947a01173b53ad3b58d44eeef4a0c96b8.jpg',
+			},
+			{
+				url: 'https://c.dns-shop.ru/thumb/st4/fit/wm/2000/2000/3d0a7868eb257a8832e2834ebe2653fe/ca500a30fad392e4c09034d083386bc40084c7fb6ee1a42a6b230f1fde79b10b.jpg',
+				source: 'ca500a30fad392e4c09034d083386bc40084c7fb6ee1a42a6b230f1fde79b10b.jpg',
+			},
+			{
+				url: 'https://c.dns-shop.ru/thumb/st4/fit/wm/2000/2000/922e2c91dbe6c1f65d970f341f69f3fd/639aa4e6ead10c10474b984e06c4ff391d6f0a3fc932468694f73570521ee5a0.jpg',
+				source: '639aa4e6ead10c10474b984e06c4ff391d6f0a3fc932468694f73570521ee5a0.jpg',
+			},
+			{
+				url: 'https://c.dns-shop.ru/thumb/st4/fit/wm/2000/2000/9805bd0089d912f002bd4eaee15730af/5bda26616bbc24ecc6329a1b3b9f60027d2e9e4e96fd60b0ad0c57ef1d813204.jpg',
+				source: '5bda26616bbc24ecc6329a1b3b9f60027d2e9e4e96fd60b0ad0c57ef1d813204.jpg',
+			},
+		],
+		name: '9.6" Планшет Dexp Ursus S290 32 ГБ 3G черный',
+		price: 86,
+		rating: 5,
+	},
+	{
+		_id: 'mys-provodnaa-steelseries-rival-600-dota-2-edition-cernyj',
+		feedbacksCount: 2,
+		subCategory: 'periferiya-i-aksessuary',
+		images: [
+			{
+				url: 'https://c.dns-shop.ru/thumb/st4/fit/wm/802/859/78449062c8d3c626a88269aa1cc5b581/976bc982e29581bbd2a1c96d96e90e711ddbc79465969f00f0bc88816114a9ff.jpg',
+				source: '976bc982e29581bbd2a1c96d96e90e711ddbc79465969f00f0bc88816114a9ff.jpg',
+			},
+			{
+				url: 'https://c.dns-shop.ru/thumb/st4/fit/wm/2000/2000/7025763744fb75f1d530542f15268692/1f8410e6355835e11e19f46f996e9635b6558d1b69206bc3f913d37361d5b268.jpg',
+				source: '1f8410e6355835e11e19f46f996e9635b6558d1b69206bc3f913d37361d5b268.jpg',
+			},
+			{
+				url: 'https://c.dns-shop.ru/thumb/st1/fit/wm/2000/2000/9896fc340fc6677330cdd5acfa510e57/f0512b080dbdf25f3c876ae24eca1128625d68e0de71efab6cc94908564fed51.jpg',
+				source: 'f0512b080dbdf25f3c876ae24eca1128625d68e0de71efab6cc94908564fed51.jpg',
+			},
+			{
+				url: 'https://c.dns-shop.ru/thumb/st4/fit/wm/2000/2000/e7b69b4f34e2c1c1f4bb7202b1ba5f17/fcdca1ceef510d048316be3fbfce9dabfbf1e9f1ba74a3cc18183557cf90a459.jpg',
+				source: 'fcdca1ceef510d048316be3fbfce9dabfbf1e9f1ba74a3cc18183557cf90a459.jpg',
+			},
+			{
+				url: 'https://c.dns-shop.ru/thumb/st1/fit/wm/2000/2000/b1728019538f34e7097eb0bc500284ba/b0a2fd52ea3043736e92179d73e888a915bda12eac59e73c69bbc2275e1f452c.jpg',
+				source: 'b0a2fd52ea3043736e92179d73e888a915bda12eac59e73c69bbc2275e1f452c.jpg',
+			},
+			{
+				url: 'https://c.dns-shop.ru/thumb/st4/fit/wm/2000/2000/5114ba99ac2f6b306f5c5597bd5e2940/4531f6ce799ffc72c203afd0aaf2a703f938b6ec8aacee04457153999bf6f085.jpg',
+				source: '4531f6ce799ffc72c203afd0aaf2a703f938b6ec8aacee04457153999bf6f085.jpg',
+			},
+			{
+				url: 'https://c.dns-shop.ru/thumb/st4/fit/wm/2000/2000/8e9336b979069809d81a373793ea09ef/fd7ba23148403a6f52807c2f38bd000a7f384a0e5a1a727542f79a1764bb143c.jpg',
+				source: 'fd7ba23148403a6f52807c2f38bd000a7f384a0e5a1a727542f79a1764bb143c.jpg',
+			},
+			{
+				url: 'https://c.dns-shop.ru/thumb/st4/fit/wm/2000/2000/c812b9ce1a5178303e06b7e32c798b81/b5f3210b75492622cab1849bd70c1471f94f4c00bb828e493ab2f41201bf4f80.jpg',
+				source: 'b5f3210b75492622cab1849bd70c1471f94f4c00bb828e493ab2f41201bf4f80.jpg',
+			},
+		],
+		name: 'Мышь проводная SteelSeries Rival 600 Dota 2 Edition черный',
+		price: 105,
+		rating: 4.5,
+	},
+	{
+		_id: '50-127-sm-televizor-led-harper-50u750ts-cernyj',
+		feedbacksCount: 2,
+		subCategory: 'televizory-i-aksessuary',
+		images: [
+			{
+				url: 'https://c.dns-shop.ru/thumb/st1/fit/0/0/7565de80365640286bff48f2a90e03b3/91bc81cc570f55e0fe771ceb78330d24520d612e8207989707942b512b14c5d1.jpg',
+				source: '91bc81cc570f55e0fe771ceb78330d24520d612e8207989707942b512b14c5d1.jpg',
+			},
+			{
+				url: 'https://c.dns-shop.ru/thumb/st1/fit/0/0/a41a18af4c8ba73e03b94d6c581dd2b6/f296ee80163f5b0c924cbdaf6dd99c31d7dbfa7ac4b1d1d6a0a07ec242478935.jpg',
+				source: 'f296ee80163f5b0c924cbdaf6dd99c31d7dbfa7ac4b1d1d6a0a07ec242478935.jpg',
+			},
+			{
+				url: 'https://c.dns-shop.ru/thumb/st1/fit/0/0/a134087f15f5c652804528f1a23eb233/55e0bbc5092d3d4649aaa3b1697d89935e5ec056e809faab0c8a73b2a4a6cf57.jpg',
+				source: '55e0bbc5092d3d4649aaa3b1697d89935e5ec056e809faab0c8a73b2a4a6cf57.jpg',
+			},
+		],
+		name: '50" (127 см) Телевизор LED Harper 50U750TS черный',
+		price: 400,
+		rating: 4.5,
+	},
+	{
+		_id: '101-planset-lenovo-tab-p10-tb-x705l-32-gb-3g-lte-belyj',
+		feedbacksCount: 4,
+		subCategory: 'planshety-elektronnye-knigi',
+		images: [
+			{
+				url: 'https://c.dns-shop.ru/thumb/st4/fit/0/0/29a12db849d89c29dd5d47a1cd6d4e2d/7d51a46e3b127ac66e2225cd0090403054f0d9ee0596d27b78bf1ec2d403972d.jpg',
+				source: '7d51a46e3b127ac66e2225cd0090403054f0d9ee0596d27b78bf1ec2d403972d.jpg',
+			},
+			{
+				url: 'https://c.dns-shop.ru/thumb/st1/fit/wm/2000/2000/5535450ba0554cf35ccf0b1b8dfeabcb/b3f6ca97c802b88a7b0461d2adc92c056d944a3f0efee203e28e1dcce9d8691d.jpg',
+				source: 'b3f6ca97c802b88a7b0461d2adc92c056d944a3f0efee203e28e1dcce9d8691d.jpg',
+			},
+			{
+				url: 'https://c.dns-shop.ru/thumb/st4/fit/wm/2000/2000/321493e7aeb1240f01a18d697ef0f8fb/daf123cdeaf8ca466c404409a7c21a3de021b6c36b5421a19d26d3e5c0a9ba3a.jpg',
+				source: 'daf123cdeaf8ca466c404409a7c21a3de021b6c36b5421a19d26d3e5c0a9ba3a.jpg',
+			},
+			{
+				url: 'https://c.dns-shop.ru/thumb/st4/fit/wm/2000/2000/265a5f28a22aa51517085f865d68d754/0c8bb48d6b5ab51fef3893ad2c97c202e63be064a941050e58b77ab6255aa1fb.jpg',
+				source: '0c8bb48d6b5ab51fef3893ad2c97c202e63be064a941050e58b77ab6255aa1fb.jpg',
+			},
+			{
+				url: 'https://c.dns-shop.ru/thumb/st4/fit/wm/2000/2000/a88739ef31bc9360cd5ae43ede83c535/b7b482a7f9987ec97cdab9b24dbfe6f2d6998d6ff2c4e918f3872526fa1e25bd.jpg',
+				source: 'b7b482a7f9987ec97cdab9b24dbfe6f2d6998d6ff2c4e918f3872526fa1e25bd.jpg',
+			},
+			{
+				url: 'https://c.dns-shop.ru/thumb/st1/fit/wm/2000/2000/e14d0190ecefdc29c2bd3614eb7534d9/86d8a8744bb77b816a65498f10a4a23b4384f868867de7e9f36ec2dd895c4009.jpg',
+				source: '86d8a8744bb77b816a65498f10a4a23b4384f868867de7e9f36ec2dd895c4009.jpg',
+			},
+			{
+				url: 'https://c.dns-shop.ru/thumb/st4/fit/wm/2000/2000/2f55e6a2f495bcab783702e5844bdff1/c49577bd94554476639c995f6e83429fb94c362e77010e9cbfd7858a18635bf3.jpg',
+				source: 'c49577bd94554476639c995f6e83429fb94c362e77010e9cbfd7858a18635bf3.jpg',
+			},
+			{
+				url: 'https://c.dns-shop.ru/thumb/st4/fit/wm/2000/2000/679e83cee1e7261341701c92e57a8634/42ff8615619bff7c1ad5bed8f4dd8aac335bde5a193f075ffabe095be176206b.jpg',
+				source: '42ff8615619bff7c1ad5bed8f4dd8aac335bde5a193f075ffabe095be176206b.jpg',
+			},
+			{
+				url: 'https://c.dns-shop.ru/thumb/st1/fit/wm/2000/2000/b56b57e4ea24f9af67c1aae1373bb6f2/ed77920ba1912ab1617426e83b3395694709130141d0e1e617e83a46d6867548.jpg',
+				source: 'ed77920ba1912ab1617426e83b3395694709130141d0e1e617e83a46d6867548.jpg',
+			},
+		],
+		name: '10.1" Планшет Lenovo TAB P10 TB-X705L 32 ГБ 3G, LTE белый',
+		price: 300,
+		rating: 3.75,
+	},
+	{
+		_id: 'iso-konnektor-intro-iso-ns-02',
+		feedbacksCount: 2,
+		subCategory: 'avtozvuk',
+		images: [
+			{
+				url: 'https://c.dns-shop.ru/thumb/st1/fit/wm/1908/1294/c933775808fe90b652df11d917e8eeb2/ae0f31af8ac3edcd0a4d69501eb7a2654137cc7b616febeb60f31e6102120ce6.jpg',
+				source: 'ae0f31af8ac3edcd0a4d69501eb7a2654137cc7b616febeb60f31e6102120ce6.jpg',
+			},
+			{
+				url: 'https://c.dns-shop.ru/thumb/st4/fit/wm/2000/1500/93b668871017bca4df3cb7da17266364/ea566f11c6e80c86b2f9ac62b231d178d4bbea5c61ef1ba81fd40c232f76895c.jpg',
+				source: 'ea566f11c6e80c86b2f9ac62b231d178d4bbea5c61ef1ba81fd40c232f76895c.jpg',
+			},
+			{
+				url: 'https://c.dns-shop.ru/thumb/st4/fit/wm/2000/1500/3ca4cdf1679c65a3a56049ddb961033f/9a938d4a5cb6509e30b409dcee8e87d2dc6059304ef7efbd8ce62db5538c9d96.jpg',
+				source: '9a938d4a5cb6509e30b409dcee8e87d2dc6059304ef7efbd8ce62db5538c9d96.jpg',
+			},
+			{
+				url: 'https://c.dns-shop.ru/thumb/st4/fit/wm/1500/2000/bffa6023df9e08af45535c7545943e69/72516a90d30ca55a1c18efa39632f0023b0c1f526735e1a4ae0abcf503a1b85d.jpg',
+				source: '72516a90d30ca55a1c18efa39632f0023b0c1f526735e1a4ae0abcf503a1b85d.jpg',
+			},
+		],
+		name: 'ISO-коннектор Intro ISO NS-02',
+		price: 5,
+		rating: 4,
+	},
+	{
+		_id: 'analogovaa-kamera-hikvision-hiwatch-ds-t209p-28-12mm',
+		feedbacksCount: 5,
+		subCategory: 'professionalnoe-setevoe-oborudovanie',
+		images: [
+			{
+				url: 'https://c.dns-shop.ru/thumb/st4/fit/wm/2000/2000/c0387caccca024a3459c16159be6e55e/62091d7e2badbae91132820570b9bccfd0bb7a8f768f2514e208e4b4d618210f.jpg',
+				source: '62091d7e2badbae91132820570b9bccfd0bb7a8f768f2514e208e4b4d618210f.jpg',
+			},
+			{
+				url: 'https://c.dns-shop.ru/thumb/st1/fit/wm/2000/2000/510bbc71689b1af5cd0d3e6f1b748f2b/dc6f62c309c5cdb8eecc96715037422447853f3b0f6bdef3a86243079f9851f3.jpg',
+				source: 'dc6f62c309c5cdb8eecc96715037422447853f3b0f6bdef3a86243079f9851f3.jpg',
+			},
+			{
+				url: 'https://c.dns-shop.ru/thumb/st4/fit/wm/2000/2000/5997dbc3365bd359c4669b2d7d0edb29/37ea396d7bff0852a385b17569c860aa3bd75bcdb34e00f979afe4ac9b4095ab.jpg',
+				source: '37ea396d7bff0852a385b17569c860aa3bd75bcdb34e00f979afe4ac9b4095ab.jpg',
+			},
+			{
+				url: 'https://c.dns-shop.ru/thumb/st1/fit/wm/2000/2000/c910f8941231defb62013ca7707db1a1/d2b5aad78f28de4362210c08c89fc3ff413276fad3cc73c270a6c99105eb41b6.jpg',
+				source: 'd2b5aad78f28de4362210c08c89fc3ff413276fad3cc73c270a6c99105eb41b6.jpg',
+			},
+			{
+				url: 'https://c.dns-shop.ru/thumb/st1/fit/wm/1999/2000/7b22ecca5872df6c25e5285b0179883c/e13bda09fa6f38bef6b91ff57eb5892e39123725f1214afa2d68d36fca1f85e9.jpg',
+				source: 'e13bda09fa6f38bef6b91ff57eb5892e39123725f1214afa2d68d36fca1f85e9.jpg',
+			},
+			{
+				url: 'https://c.dns-shop.ru/thumb/st4/fit/wm/2000/2000/16172b7ca80941c6797d27f1e714be37/1b04eb8df2ab82e52a92485a9c94e0f579cefa8efb220820ade65f7b0566ed29.jpg',
+				source: '1b04eb8df2ab82e52a92485a9c94e0f579cefa8efb220820ade65f7b0566ed29.jpg',
+			},
+			{
+				url: 'https://c.dns-shop.ru/thumb/st1/fit/wm/2000/2000/b1385f4b28b8097093c42168d61b7ed5/bc7db1ab1fedd14999db74c3dda1ab6cdd7d800040266748978329c2c41eb396.jpg',
+				source: 'bc7db1ab1fedd14999db74c3dda1ab6cdd7d800040266748978329c2c41eb396.jpg',
+			},
+		],
+		name: 'Аналоговая камера Hikvision HiWatch DS-T209P 2.8-12мм',
+		price: 83,
+		rating: 4.8,
+	},
+	{
+		_id: 'kabel-dexp-hdmi---hdmi-1-m',
+		feedbacksCount: 2,
+		subCategory: 'televizory-i-aksessuary',
+		images: [
+			{
+				url: 'https://c.dns-shop.ru/thumb/st4/fit/wm/2000/1500/2d2b2b6b3bb6b3f45220cddc96b564b4/982fda86a5ce0697268440b04617e06afa26b62ccecca4a0b3cbd38bc5e81b1f.jpg',
+				source: '982fda86a5ce0697268440b04617e06afa26b62ccecca4a0b3cbd38bc5e81b1f.jpg',
+			},
+			{
+				url: 'https://c.dns-shop.ru/thumb/st4/fit/wm/2000/1500/a36f7e0945e780dbd101858f31083ff7/5a4984cbb35c862f3ec262bb92e34810460a895d22f4e5c5df84d9f83d09e97c.jpg',
+				source: '5a4984cbb35c862f3ec262bb92e34810460a895d22f4e5c5df84d9f83d09e97c.jpg',
+			},
+			{
+				url: 'https://c.dns-shop.ru/thumb/st4/fit/wm/1500/2000/e769c7aff544ea133f7410d82c9d722f/7667699e45f4bd311023d66255c77237c677dafc65664020f56e13b50ac5f090.jpg',
+				source: '7667699e45f4bd311023d66255c77237c677dafc65664020f56e13b50ac5f090.jpg',
+			},
+			{
+				url: 'https://c.dns-shop.ru/thumb/st4/fit/wm/1500/2000/f17f5ebc75f3cf7788fdcb85a4605f29/91fed0937cc8c516a6705f6445bbb080faa33c9c07d9f4104b2cc5029ce07635.jpg',
+				source: '91fed0937cc8c516a6705f6445bbb080faa33c9c07d9f4104b2cc5029ce07635.jpg',
+			},
+		],
+		name: 'Кабель DEXP HDMI - HDMI, 1 м',
+		price: 8,
+		rating: 4.5,
+	},
+	{
+		_id: '65-165-sm-televizor-led-lg-65sm9800pla-cernyj',
+		feedbacksCount: 4,
+		subCategory: 'televizory-i-aksessuary',
+		images: [
+			{
+				url: 'https://c.dns-shop.ru/thumb/st1/fit/0/0/44cc1bd15e5166bd6765758d20df1174/350e95792b1ef6486916b896585367b897681966865ba7343e698a53e80f33a3.jpg',
+				source: '350e95792b1ef6486916b896585367b897681966865ba7343e698a53e80f33a3.jpg',
+			},
+			{
+				url: 'https://c.dns-shop.ru/thumb/st1/fit/wm/2000/2000/26d9b2e7bf151a91830ccc96bb50010b/963037dde578dfda0dc4646c69c1d85ade1a5dd0a05ceec4d655bfa1b63e722e.jpg',
+				source: '963037dde578dfda0dc4646c69c1d85ade1a5dd0a05ceec4d655bfa1b63e722e.jpg',
+			},
+			{
+				url: 'https://c.dns-shop.ru/thumb/st4/fit/wm/2000/2000/9122557f6d2d3674b0d1395e8b21fc80/3182a8e77c7d07818298ad4d6dc4f818bdf7bf612182b7534a18e754f7d8592f.jpg',
+				source: '3182a8e77c7d07818298ad4d6dc4f818bdf7bf612182b7534a18e754f7d8592f.jpg',
+			},
+			{
+				url: 'https://c.dns-shop.ru/thumb/st4/fit/wm/2000/2000/1b0397862b2c157784cdd69840e09cee/45d22ba4c4a7825c0dd7660c2698e7ee9664990ebec5cf4a11b28c67fcc0fcd9.jpg',
+				source: '45d22ba4c4a7825c0dd7660c2698e7ee9664990ebec5cf4a11b28c67fcc0fcd9.jpg',
+			},
+			{
+				url: 'https://c.dns-shop.ru/thumb/st4/fit/wm/2000/2000/4386bbbcef700382d870fc0718bcc1f3/f5a32b4cf11218146026fa10112cd840f8abf712f7ce1117be76ba4056971af0.jpg',
+				source: 'f5a32b4cf11218146026fa10112cd840f8abf712f7ce1117be76ba4056971af0.jpg',
+			},
+			{
+				url: 'https://c.dns-shop.ru/thumb/st4/fit/wm/2000/2000/736833b04ae43a8eaef51c6566ee34f3/2ad639e128664af1fc1c79cfe704d372659440ae1d056bafaceaa95cb03f2454.jpg',
+				source: '2ad639e128664af1fc1c79cfe704d372659440ae1d056bafaceaa95cb03f2454.jpg',
+			},
+			{
+				url: 'https://c.dns-shop.ru/thumb/st1/fit/wm/2000/2000/ec7bf91ff62a3b8a6f5c7f88fad370e9/a0cf9b770fa418feb462a5d4995b03ab2df2ce56dbd5dbb3f45f5df2490a7393.jpg',
+				source: 'a0cf9b770fa418feb462a5d4995b03ab2df2ce56dbd5dbb3f45f5df2490a7393.jpg',
+			},
+			{
+				url: 'https://c.dns-shop.ru/thumb/st4/fit/wm/2000/2000/7db0b6da255030c53cc3a5c9b2ccf804/af7c0335af69bf3eeb8fb5fbb87afd2cb38702166d781334e71aefb8855b842d.jpg',
+				source: 'af7c0335af69bf3eeb8fb5fbb87afd2cb38702166d781334e71aefb8855b842d.jpg',
+			},
+			{
+				url: 'https://c.dns-shop.ru/thumb/st1/fit/wm/2000/2000/9bed5480f80b1c412d719a0ee4c33e97/f543e3a1d82f63cc87606a0b8726ae2ee939e496f678b1ab6b124a1d42602f15.jpg',
+				source: 'f543e3a1d82f63cc87606a0b8726ae2ee939e496f678b1ab6b124a1d42602f15.jpg',
+			},
+		],
+		name: '65" (165 см) Телевизор LED LG 65SM9800PLA черный',
+		price: 2286,
+		rating: 4.75,
+	},
+	{
+		_id: 'vitaa-para-skynet-csl-ftp-4-cu',
+		feedbacksCount: 5,
+		subCategory: 'professionalnoe-setevoe-oborudovanie',
+		images: [
+			{
+				url: 'https://c.dns-shop.ru/thumb/st1/fit/wm/2000/1292/f1da10dcd92bc5800ad808a443c1c57b/a67f341c37067c12bb959fb5bcfd95288e973fe7cef6ef4e262d517cd382620f.jpg',
+				source: 'a67f341c37067c12bb959fb5bcfd95288e973fe7cef6ef4e262d517cd382620f.jpg',
+			},
+			{
+				url: 'https://c.dns-shop.ru/thumb/st1/fit/wm/1500/2000/91a43fdccf9c83937ff6d72b9be34e62/f5367f92fe34dfe2cdfbc16d6a5d6d90ae100e5550ddba068631468a47506f48.jpg',
+				source: 'f5367f92fe34dfe2cdfbc16d6a5d6d90ae100e5550ddba068631468a47506f48.jpg',
+			},
+			{
+				url: 'https://c.dns-shop.ru/thumb/st1/fit/wm/2000/1500/7153b01ca5f9b31832191a43ca0853f4/0858cc144b2d2ed4ee2b05b98a961d8288579efbb90c5ac0e1f6ec0c5ef4f7a4.jpg',
+				source: '0858cc144b2d2ed4ee2b05b98a961d8288579efbb90c5ac0e1f6ec0c5ef4f7a4.jpg',
+			},
+		],
+		name: 'Витая пара SkyNet CSL-FTP-4-CU',
+		price: 81,
+		rating: 4.4,
+	},
+];
