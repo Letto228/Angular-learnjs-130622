ff6a97617961ccd7b6e92df662487a5602219118
Rahib Babayev <rahibwebdeveloper@gmail.com>
2022-06-25 00:33:35 +0400

    feat: refactoring

--- src/app/components/sidenav/sidenav.component.ts
+++ src/app/components/sidenav/sidenav.component.ts
@@ -1,13 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroupDirective, NgForm, Validators } from '@angular/forms';
-import { ErrorStateMatcher } from '@angular/material/core';
-
-export class MyErrorStateMatcher implements ErrorStateMatcher {
-	isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
-		const isSubmitted = form && form.submitted;
-		return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
-	}
-}
+import { Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { MyErrorStateMatcher } from '../../models/errorStateMatcher.model';
 
 @Component({
 	selector: 'app-sidenav',
