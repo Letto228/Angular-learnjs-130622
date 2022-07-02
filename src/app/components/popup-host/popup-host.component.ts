1bb94b09b2c69369e3b848a7ef3751b99ace0826
Rahib Babayev <rahibwebdeveloper@gmail.com>
2022-07-02 18:15:37 +0400

    feat: created popup-host component

---
+++ src/app/components/popup-host/popup-host.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'app-popup-host',
+	templateUrl: './popup-host.component.html',
+	styleUrls: ['./popup-host.component.less'],
+})
+export class PopupHostComponent implements OnInit {
+	constructor() {}
+
+	ngOnInit(): void {}
+}
---
+++ src/app/components/popup-host/popup-host.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PopupHostComponent } from './popup-host.component';
+
+@NgModule({
+	declarations: [PopupHostComponent],
+	imports: [CommonModule],
+})
+export class PopupHostModule {}
