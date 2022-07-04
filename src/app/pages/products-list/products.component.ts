c5d8cbe43b3569943ce33230cd34f253c5eb2b7b
Letto228 <learnjs@MacBook-Pro-Egor.local>
2022-07-04 19:32:49 +0300

    chore: review pre lesson 6

--- src/app/pages/products-list/products.component.ts
+++ src/app/pages/products-list/products.component.ts
@@ -1,6 +1,5 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { productsMock } from '../../shared/products/products.mock';
-import { LoadDirection } from '../../shared/scroll-with-loading/enums';
 
 @Component({
 	selector: 'app-products',
@@ -10,32 +9,4 @@ import { LoadDirection } from '../../shared/scroll-with-loading/enums';
 })
 export class ProductsComponent {
 	readonly products = productsMock;
-	isLoadingData = false;
-
-	constructor(private changeDetectorRef: ChangeDetectorRef) {}
-
-	onLoadData(direction: LoadDirection) {
-		console.log(direction);
-
-		//сэмитируем в этом методе подгрузку данных
-		this.isLoadingData = true;
-
-		setTimeout(() => {
-			this.isLoadingData = false;
-
-			this.changeDetectorRef.markForCheck();
-		}, 3000);
-	}
-	// isHovered = true;
-
-	// constructor(
-	// 	private changeDetectorRef: ChangeDetectorRef,
-	// ) {}
-
-	// ngOnInit() {
-	// 	setTimeout(() => {
-	// 		this.isHovered = false;
-	// 		this.changeDetectorRef.markForCheck();
-	// 	}, 3000)
-	// }
 }
--- src/app/pages/products-list/products.module.ts
+++ src/app/pages/products-list/products.module.ts
@@ -5,12 +5,10 @@ import { ProductCardComponent } from './product-card/product-card.component';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { NgClassModule } from '../../shared/ng-class/ng-class.module';
-import { ScrollWithLoadingModule } from 'src/app/shared/scroll-with-loading/scroll-with-loading.module';
 
 @NgModule({
 	declarations: [ProductsComponent, ProductCardComponent],
-	imports: [CommonModule, MatCardModule, MatButtonModule, MatIconModule, NgClassModule, ScrollWithLoadingModule],
+	imports: [CommonModule, MatCardModule, MatButtonModule, MatIconModule],
 	exports: [ProductsComponent],
 })
 export class ProductsModule {}
