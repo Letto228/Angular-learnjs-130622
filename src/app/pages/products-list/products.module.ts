ac68d53185629843f1b9077d7a1acf312b78490d
Rahib Babayev <rahibwebdeveloper@gmail.com>
2022-07-11 01:58:57 +0400

    feat: created logic for scroll-load

--- src/app/pages/products-list/products.module.ts
+++ src/app/pages/products-list/products.module.ts
@@ -6,10 +6,11 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { NgClassModule } from '../../shared/ng-class/ng-class.module';
+import { ScrollLoadModule } from '../../shared/scroll-load/scroll-load.module';
 
 @NgModule({
 	declarations: [ProductsComponent, ProductCardComponent],
-	imports: [CommonModule, MatCardModule, MatButtonModule, MatIconModule, NgClassModule],
+	imports: [CommonModule, MatCardModule, MatButtonModule, MatIconModule, NgClassModule, ScrollLoadModule],
 	exports: [ProductsComponent],
 })
 export class ProductsModule {}
--- src/app/shared/scroll-load/scroll-load.directive.ts
+++ src/app/shared/scroll-load/scroll-load.directive.ts
@@ -1,8 +1,45 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, EventEmitter, HostListener, Input, Output } from '@angular/core';
+import { Direction } from './direction.enum';
 
 @Directive({
-	selector: '[appScrollLoad]',
+	selector: '[scrollLoad]',
 })
 export class ScrollLoadDirective {
-	constructor() {}
+	@Input() isLoadingData: boolean = false;
+
+	@Output() scrollLoadEvent = new EventEmitter<Direction>();
+
+	scrollPosition: number = 0;
+	offset: number = 100;
+
+	constructor(private elementRef: ElementRef) {}
+
+	@HostListener('scroll')
+	onScrollLoad() {
+		if (this.isLoadingData) {
+			return;
+		}
+
+		if (this.calculateScroll) {
+			this.scrollLoadEmitting(Direction.BEFORE);
+		} else if (this.scrollTop > this.scrollPosition) {
+			this.scrollLoadEmitting(Direction.AFTER);
+		}
+	}
+
+	private scrollLoadEmitting(direction: Direction) {
+		this.scrollLoadEvent.emit(direction);
+	}
+
+	private get nativeElement(): HTMLElement {
+		return this.elementRef.nativeElement;
+	}
+
+	private get scrollTop(): number {
+		return this.nativeElement.scrollTop;
+	}
+
+	private get calculateScroll(): boolean {
+		return this.nativeElement.scrollHeight - this.scrollTop < this.nativeElement.clientHeight + this.offset;
+	}
 }
