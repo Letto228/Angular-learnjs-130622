bdfc1afad3285bcace98bf6e99b2ef186747b539
Sergey Kamenev <dexvoid@yandex.ru>
2022-07-17 12:54:51 +0700

    feat: add img carousel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,23 @@ import { Component } from '@angular/core';
 export class AppComponent {
 	title = 'Angular-learnjs-130622';
 	titleSecret = 'Angular-learnjs-130622-Secret';
+  private iterator = 0;
 
-	imgSrc =
-		'https://get.wallhere.com/photo/landscape-sea-reflection-bridge-suspension-bridge-1680x1050-px-nonbuilding-structure-cable-stayed-bridge-truss-bridge-706446.jpg';
+	imgSrc = [
+    'https://img.freepik.com/free-photo/closeup-beautiful-green-leaves_23-2148245094.jpg?w=1380',
+    'https://img.freepik.com/free-photo/top-view-small-sour-blue-black-sloe-bucket-with-leaves-grey-background-with-copy-space-jpg_141793-20509.jpg?w=1380',
+    'https://img.freepik.com/free-photo/top-view-kumquat-oat-flakes-with-copy-space-yellow-jpg_140725-12394.jpg?w=1380'
+  ]
+  constructor() {
+    this.startChanging();
+  }
+  getImg() {
+    return this.imgSrc[this.iterator];
+  }
+  startChanging() {
+    setInterval(() => {
+      this.iterator = this.iterator === this.imgSrc.length - 1 ? 0 : this.iterator + 1;
+    }, 2000);
+  }
+
 }
